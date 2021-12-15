da799e3ee6c62d15f0396b32ae349352dd40ce8d
thugoo <thugomail@gmail.com>
2021-12-15 23:44:18 +0200

    MULTIPLAYER :DD

--- mäng.py
+++ mäng.py
@@ -1,6 +1,7 @@
 import pygame
 import time
 from random import randint
+from network import Network
 
 pygame.init()
 
@@ -33,22 +34,49 @@ def doll(sprite, x, y):
 green_sound = pygame.mixer.Sound("greenlight.mp3")
 red_sound = pygame.mixer.Sound("redlight.mp3")
 
+## PLAYER
+f1 = (1,pygame.image.load('otse0.png'))
+f2 = (2,pygame.image.load("otse1.png"))
+f3 = (3,pygame.image.load("otse2.png"))
+r1 = (4,pygame.image.load("parem0.png"))
+r2 = (5,pygame.image.load("parem1.png"))
+r3 = (6,pygame.image.load("parem2.png"))
+l1 = (7,pygame.image.load("vasak0.png"))
+l2 = (8,pygame.image.load("vasak1.png"))
+l3 = (9,pygame.image.load("vasak2.png"))
+b1 = (10,pygame.image.load("tagasi0.png"))
+b2 = (11,pygame.image.load("tagasi1.png"))
+b3 = (12,pygame.image.load("tagasi2.png"))
+
 ## player sprite
-playerIcon = pygame.image.load('otse0.png')
-forward = [pygame.image.load("otse0.png"), pygame.image.load("otse1.png"), pygame.image.load("otse2.png")]
-right= [pygame.image.load("parem0.png"), pygame.image.load("parem1.png"), pygame.image.load("parem2.png")]
-left = [pygame.image.load("vasak0.png"), pygame.image.load("vasak1.png"), pygame.image.load("vasak2.png")]
-backwards = [pygame.image.load("tagasi0.png"), pygame.image.load("tagasi1.png"), pygame.image.load("tagasi2.png")]
+playerIcon = f1
+forward = [f1, f2, f3]
+right = [r1, r2, r3]
+left = [l1, l2, l3]
+backwards = [b1, b2, b3]
 i = 0
 
+p2Images = [f1,f2,f3,r1,r2,r3,l1,l2,l3,b1,b2,b3]
+
+p1Current = 0
+p2Current = 0
+player2Icon = p2Images[p2Current]
+
 ## player coordinates
-playerX = randint(0, 945)
-playerY = 700
+player2X = 0
+player2Y = 0
 change = 0.06
 
 ## player function
-def player(x, y):
-    screen.blit(playerIcon, (x, y))
+def player(sprite, x, y):
+    screen.blit(sprite, (x, y))
+
+def read_pos(s):
+    s = s.split(",")
+    return int(s[0]), float(s[1]), float(s[2]), int(s[3])
+
+def make_pos(sprite ,x, y, value):
+    return str(sprite) + ',' + str(x) + ',' + str(y) + ',' + str(value)
 
 ## background
 background = pygame.image.load("background.png")
@@ -57,18 +85,27 @@ background = pygame.image.load("background.png")
 win = pygame.image.load("win.png")
 lose = pygame.image.load("lose.png")
 
-# red light / green light switch
-lightSwitch = pygame.USEREVENT + 0
-pygame.time.set_timer(lightSwitch, randint(2500,10000))
-
 startTime = time.time()
 didPlayerLose = False
 
 ## game loop
 running = True
+n = Network()
+startPos = n.getPos().split(',')
+player1X = float(startPos[1])
+player1Y = float(startPos[2])
+
 while running:
-    
-    if playerY < 89:
+
+    for x in range(0, len(p2Images)):
+        if playerIcon[0] in p2Images[x]:
+            p1Current = p2Images[x][0]
+
+    p2Pos = read_pos(n.send(make_pos(p1Current, player1X, player1Y, int(isItGreen))))
+    player2X = p2Pos[1]
+    player2Y = p2Pos[2]
+
+    if player1Y < 89:
 
         screen.fill((0,0,0))
         screen.blit(win, (0,0))
@@ -76,18 +113,18 @@ while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-        
+
         pygame.display.update()
 
     elif didPlayerLose:
-        
+
         screen.fill((0,0,0))
         screen.blit(lose, (0,0))
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-        
+
         pygame.display.update()
 
     else:
@@ -99,61 +136,67 @@ while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-            if event.type == lightSwitch:
-                pygame.time.set_timer(lightSwitch, randint(2500, 10000))
-                isItGreen = not isItGreen
-                if isItGreen:
-                    pygame.mixer.Sound.play(green_sound)
-                elif not isItGreen:
-                    startTime = round(time.time(), 1)
-                    pygame.mixer.Sound.play(red_sound)
-        
+
+        print(p2Pos)
+        if p2Pos[3] != int(isItGreen):
+            isItGreen = not isItGreen
+            if isItGreen:
+                pygame.mixer.Sound.play(green_sound)
+            elif not isItGreen:
+                startTime = round(time.time(), 1)
+                pygame.mixer.Sound.play(red_sound)
+
         key = pygame.key.get_pressed()
-        
+
         if not isItGreen:
             if round(time.time(), 1) > float(startTime) + 0.5:
                 if key[pygame.K_LEFT] or key[pygame.K_RIGHT] or key[pygame.K_UP] or key[pygame.K_DOWN]:
                     didPlayerLose = True
 
         if key[pygame.K_LEFT]:
-            playerX -= change
+            player1X -= change
             i += 0.01
             if i >= len(left):
                 i = 0
-            playerIcon= left[int(i)]
+            playerIcon = left[int(i)]
         elif key[pygame.K_RIGHT]:
-            playerX += change
+            player1X += change
             i += 0.01
             if i >= len(right):
                 i = 0
-            playerIcon= right[int(i)]
+            playerIcon = right[int(i)]
         elif key[pygame.K_UP]:
-            playerY -= change
+            player1Y -= change
             i += 0.01
             if i >= len(forward):
                 i = 0
-            playerIcon= forward[int(i)]
+            playerIcon = forward[int(i)]
         elif key[pygame.K_DOWN]:
-            playerY += change
+            player1Y += change
             fw = False
             i += 0.01
             if i >= len(backwards):
                 i = 0
-            playerIcon= backwards[int(i)]
-        if playerX <= 0:
-            playerX = 0
-        elif playerX >= 975:
-            playerX = 975
-        
-        if playerY <= 0:
-            playerY = 0
-        elif playerY >= 755:
-            playerY = 755
+            playerIcon = backwards[int(i)]
+        if player1X <= 0:
+            player1X = 0
+        elif player1X >= 975:
+            player1X = 975
+
+        if player1Y <= 0:
+            player1Y = 0
+        elif player1Y >= 755:
+            player1Y = 755
 
         if isItGreen:
             doll(greenLight, dollX, dollY)
         elif not isItGreen:
             doll(redLight, dollX, dollY)
 
-        player(playerX, playerY)
+        for x in range(0, len(p2Images)):
+            if p2Pos[0] == p2Images[x][0]:
+                player2Icon = p2Images[x]
+
+        player(playerIcon[1], player1X, player1Y)
+        player(player2Icon[1], player2X, player2Y)
         pygame.display.update()
--- server.py
+++ server.py
@@ -1,6 +1,10 @@
 import socket
+import pygame
 from _thread import *
+from random import randint
 import sys
+
+pygame.init()
 
 server = '192.168.1.176'
 port = 5555
@@ -12,35 +16,64 @@ try:
 except socket.error as e:
     str(e)
 
+# red light / green light switch
+lightSwitch = pygame.USEREVENT + 1
+pygame.time.set_timer(lightSwitch, randint(2500, 10000))
+## green light or red light boolean
+isItGreen = False
+
 s.listen(2)
-print('waiting for connection, server started')
+print('Waiting for connection, server started')
 
-def threaded_client(conn):
+def read_pos(s):
+    s = s.split(",")
+    return int(s[0]), float(s[1]), float(s[2]), int(s[3])
 
-    conn.send(str.encode('connected'))
+def make_pos(tup):
+    return str(tup[0]) + ',' + str(tup[1]) + ',' + str(tup[2]) + ',' + str(tup[3])
+
+pos = [(0, randint(0, 945), 700, 0),(0, randint(0, 945), 700, 0)]
+
+def threaded_client(conn, player):
+
+    global isItGreen
+
+    conn.send(str.encode(make_pos(pos[player])))
     reply = ''
     while True:
-        try:
-            data = conn.recv(2048)
-            reply = data.decode('utf-8')
 
-            if not data:
-                print('disconnected')
-                break
-            else:
-                print('received:', reply)
-                print('sending: ', reply)
+        for event in pygame.event.get():
+            if event.type == lightSwitch:
+                pygame.time.set_timer(lightSwitch, randint(2500, 10000))
+                isItGreen = not isItGreen
 
-            conn.sendall(str.encode(reply))
+        data = read_pos(conn.recv(2048).decode())
+        data = (data[0], data[1], data[2], int(isItGreen))
+        pos[player] = (data[0], data[1], data[2], data[3])
+        reply = data
 
-        except:
+        if not data:
+            print('Disconnected')
             break
+        else:
+            if player == 1:
+                reply = pos[0]
+            else:
+                reply = pos[1]
 
-        print('Lost connection')
-        conn.close()
+            print('Received:', data)
+            print('Sending: ', reply)
+
+        conn.sendall(str.encode(make_pos(reply)))
+
+    print('Lost connection')
+    conn.close()
+
+currentPlayer = 0
 
 while True:
     conn, addr = s.accept()
-    print('connected to:', addr)
+    print('Connected to:', addr)
 
-    start_new_thread(threaded_client, (conn,))
+    start_new_thread(threaded_client, (conn, currentPlayer))
+    currentPlayer += 1
